b469dedb0eb89ba48edf13a73ba3fff9681aec1c
LaMar Brown <l94marrl@outlook.com>
2025-01-27 20:05:55 -0500

    Add project files.

---
+++ Sales Tax module.cpp
@@ -0,0 +1,36 @@
+/*
+
+File Name:SalesTaxModule.cpp
+Github URL:
+Programmer: LaMar Brown
+Date: January 2025
+Requirement
+
+Sales Tax  Write a program that will compute
+the sales tax on a 95 purchase. Assume the
+state sales tax is 4 percent, and the county 
+sales tax is 2 percent.
+answer. Display the answer.
+*/
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    int number1 = 95; 
+    int tax1 = 4;
+    int tax2 = 2;
+    int answer;
+    answer = number1 + tax1 * tax2;
+    cout << "the total of a " << number1 << " dollar purchase with " << tax1 << " percent state sales tax and " << tax2 << " percent county sales tax is" << answer << "\n";
+    return answer; 
+
+
+   
+    
+    
+}
+
+
